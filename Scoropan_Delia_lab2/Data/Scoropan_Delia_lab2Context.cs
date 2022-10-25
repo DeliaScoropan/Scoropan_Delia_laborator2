31965560ecf53d5ff246b7afcdef0eddd0bc6077
Delia <Delia@DESKTOP-04QSD0M>
2022-10-25 12:45:18 +0300

    Add project files.

---
+++ Scoropan_Delia_lab2/Data/Scoropan_Delia_lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Scoropan_Delia_lab2.Models;
+
+namespace Scoropan_Delia_lab2.Data
+{
+    public class Scoropan_Delia_lab2Context : DbContext
+    {
+        public Scoropan_Delia_lab2Context (DbContextOptions<Scoropan_Delia_lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Scoropan_Delia_lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Scoropan_Delia_lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Scoropan_Delia_lab2/Pages/Publishers/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Scoropan_Delia_lab2.Data;
+using Scoropan_Delia_lab2.Models;
+
+namespace Scoropan_Delia_lab2.Pages.Publishers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Scoropan_Delia_lab2.Data.Scoropan_Delia_lab2Context _context;
+
+        public CreateModel(Scoropan_Delia_lab2.Data.Scoropan_Delia_lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Publisher.Add(Publisher);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
